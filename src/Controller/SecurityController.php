192d198b4448449e021798660fdac57577199fee
michel michael <m.michel@onlineformapro.com>
2021-04-13 15:36:01 +0200

    Signed-off-by: michel michael <m.michel@onlineformapro.com>ajout condition log menu Signed-off-by: michel michael <m.michel@onlineformapro.com>

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,7 +27,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/deconnexion", name="app_logout")
      */
     public function logout()
     {
